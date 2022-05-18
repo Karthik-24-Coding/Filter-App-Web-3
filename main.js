713fbe4bd4a6ec206be71dd18d73d22717894ecf
Karthik-24-Coding <73743387+Karthik-24-Coding@users.noreply.github.com>
2022-05-18 12:13:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+nose_x=0;
+nose_y=0;
+
+function preload(){
+    mustache_nose=loadImage("https://i.postimg.cc/3x3QzSGq/m.png");
+    }
+    
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+    }
+
+
+    
+function draw(){
+    image(video, 0, 0, 300, 300);
+    image(mustache_nose,nose_x,nose_y,70,30);
+    }
+
+function modelLoaded() {
+    console.log('poseNet is Initialized');
+    }
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
+        nose_x=results[0].pose.nose.x-35;
+        nose_y=results[0].pose.nose.y;
+        }
+    }
+
+function take_snapshot(){
+    save('yourfilteredphoto.png');
+    }
